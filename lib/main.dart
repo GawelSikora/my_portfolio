fd9e64223547536f1c00fdae79b8c603baf87fd5
Gawel Sikora <gawel.sikora@gmail.com>
2023-07-04 20:07:25 +0100

    correct HomePage and add edit button on AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,51 +14,77 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Name\'s Porfolio'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    child: const Text('User Profile Picture'),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Name\'s Porfolio'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  color: Colors.blue,
+                  child: const Text('User Profile Picture'),
+                ),
+                Container(
+                  color: Colors.blueGrey,
+                  width: 200,
+                  height: 400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'About me',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Text about you'),
+                    ],
                   ),
-                  Container(
-                    color: Colors.blueGrey,
-                    width: 250,
-                    height: 400,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const [
-                        Text('About me'),
-                        TextField(),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(Icons.art_track),
-                      label: const Text('My Art')),
-                  ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(Icons.list),
-                      label: const Text('Other Achievements')),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.art_track),
+                    label: const Text('My Art')),
+                ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.list),
+                    label: const Text('Other Achievements')),
+              ],
+            )
+          ],
         ),
       ),
     );
